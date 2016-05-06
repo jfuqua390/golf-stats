339d4f3e2b3301d52392c87b2224fa795e3c6c6a
Jonathan Fuqua <jfuqua390@gmail.com>
2016-05-06 13:33:40 -0500

    User's name shows up in nav bar if authenticated and can logout

--- app/register/controller.js
+++ app/register/controller.js
@@ -3,6 +3,8 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   createUser(formValues) {
     const user = this.store.createRecord(`golfer`, formValues);
-    user.save();
+    user.save().then(() => {
+      this.transitionToRoute(`index`);
+    });
   },
 });
