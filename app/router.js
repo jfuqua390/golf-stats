44c51cd013be7694a2a31f65cbeb6aab4802dee8
Jonathan Fuqua <jfuqua390@gmail.com>
2016-05-13 14:29:39 -0500

    can click to go to other user profile

--- app/router.js
+++ app/router.js
@@ -16,6 +16,7 @@ Router.map(function() {
   this.route('addcourse');
   this.route('courses');
   this.route('add-score');
+  this.route('user-profile', { path: `/:id`} );
 });
 
 export default Router;
