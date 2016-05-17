934c652223f9476e8cfdd05fe0f6f2a78474c458
Jonathan Fuqua <jfuqua390@gmail.com>
2016-05-17 11:54:05 -0500

    User can edit profile

--- app/user-profile/controller.js
+++ app/user-profile/controller.js
@@ -1,19 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  session: Ember.inject.service(),
-  handicap: Ember.inject.service(`handicap`),
-
-  idInt(score) {
-    return -parseInt(score.id, 10);
-  },
-
-  editBio(user, temp) {
-    user.setProperties(temp);
-
-    user.save();
-  },
-
   addComment(score, comment) {
     const x = this.store.createRecord('comment', {
       description: comment.description,
--- app/user-profile/route.js
+++ app/user-profile/route.js
@@ -1,7 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  session: Ember.inject.service(),
+
   model({ id }) {
-    return this.store.findRecord(`golfer`, id);
+    const userId = this.get(`session.data.authenticated.userId`);
+    return Ember.RSVP.hash({
+      loggeduser: this.store.findRecord(`golfer`, userId),
+      golfer: this.store.findRecord(`golfer`, id),
+      scores: this.store.findAll(`score`),
+    });
   },
 });
